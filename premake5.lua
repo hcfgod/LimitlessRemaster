468c4a84bec630fea030de5c3653ed7f2b8db030
Keith Morgan <keithmorgan223@gmail.com>
2025-07-28 14:52:20 -0700

    Updated premake5.lua to correct indentation for the output directory definition. Enhanced GitHub Actions workflow for Linux builds by refining premake5 execution commands to ensure consistent file usage and improved logging during Makefile generation, aiding in debugging and reliability.

--- premake5.lua
+++ premake5.lua
@@ -20,7 +20,7 @@ workspace "LimitlessRemaster"
     filter "platforms:ARM64"
         architecture "ARM64"
 
-outputdir = "Build/%{cfg.shortname}-%{cfg.system}-%{cfg.platform}"
+    outputdir = "Build/%{cfg.shortname}-%{cfg.system}-%{cfg.platform}"
 
 -- Include sub-projects
 include "Limitless"
